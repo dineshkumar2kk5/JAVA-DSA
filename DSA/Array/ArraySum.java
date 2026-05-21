4efe103d16b96e84022c734ba86eff2a98df88c5
dinesh <dineshkumar001official@gmail.com>
2026-05-21 17:02:45 +0530

    array

--- DSA/Array/ArraySum.java
+++ DSA/Array/ArraySum.java
@@ -1,3 +1,23 @@
 public class ArraySum {
+
+    public static void main(String[] args) {
+        int sum =0;
+
+        int [] array={1,2,3,5,6,4,3,56,4};
+        for(int i=0;
+            i<array.length;
+            i=i+1
+
+        ){
+
+            sum=sum+array[i];
+            System.out.println(sum);
+            
+
+        }
+
+        System.out.println(sum);
+    }
+
     
 }
